c49b61a643d437a4a3af49d733bcc562244c1762
Torsten Schmitz <torsten.schmitz@i22.de>
2020-05-11 17:16:28 +0200

    adds html/js files

---
+++ app.js
@@ -0,0 +1,34 @@
+
+google.charts.load('current', {
+  'packages': ['corechart', 'line'],
+});
+google.charts.setOnLoadCallback(initialize);
+
+function initialize() {
+  var queryString = encodeURIComponent('select B, C, D limit 400 label C "°C", D "%RH"');
+  var query = new google.visualization.Query(
+    'https://docs.google.com/spreadsheets/d/1kiI686Rr_pjYtplmdAw8_KKoxr8iVitv0kwQynRuvnw/gviz/tq?sheet=Sheet1&headers=0&tq=' + queryString);
+  query.send(handleSampleDataQueryResponse);
+}
+
+function handleSampleDataQueryResponse(response) {
+  if (response.isError()) {
+    alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+    return;
+  }
+
+  var data = response.getDataTable();
+
+  var options = {
+    height: 600,
+    hAxis: {
+      title: 'Hour',
+      direction: -1
+    },
+    vAxis: {
+      title: 'Temperature'
+    }
+  };
+  var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+  chart.draw(data, options);
+}
